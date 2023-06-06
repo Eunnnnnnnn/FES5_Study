154795f79cffb1a7687f5ae5aeb9e045b643b4cb
Eunnnnnnnn <0624pjh@gmail.com>
2023-06-06 16:55:00 +0900

    23. 06. 05. 혜민님 특강 reduxtoolkit

--- React/혜민님 특강/reduxtoolkit/src/App.js
+++ React/혜민님 특강/reduxtoolkit/src/App.js
@@ -1,24 +1,47 @@
-import { useSelector, useDispatch } from 'react-redux'
-import { changeName, changeAge } from './store.js'
+// import { useSelector, useDispatch } from 'react-redux'
+// import { changeName, changeAge } from './store.js'
 
+
+// function App() {
+
+//   let state = useSelector((state) => { return state })
+//   console.log(state);
+//   console.log(state.user);
+//   console.log(state.user2);
+
+//   let dispatch = useDispatch();
+
+//   return (
+//     <div>
+//       <button onClick={() => {
+//         dispatch(changeName())
+//       }}>이름바꿔~</button>
+//       <button onClick={() => {
+//         dispatch(changeAge())
+//       }}>나이바꿔~</button>
+//     </div>
+//   );
+// }
+// export default App;
+
+
+import { useSelector, useDispatch } from "react-redux";
+import { changeName } from "./store";
 
 function App() {
 
-  let state = useSelector((state) => { return state })
-  console.log(state);
-  console.log(state.user);
-  console.log(state.user2);
+  //등록된 state 다 가져옴
+  // const state = useSelector((state) => { return state });
 
-  let dispatch = useDispatch();
+  // 등록된 state 중에서 user 가져옴
+  const state = useSelector((state) => { return state.user });
+  const dispatch = useDispatch();
 
   return (
     <div>
-      <button onClick={() => {
-        dispatch(changeName())
-      }}>이름바꿔~</button>
-      <button onClick={() => {
-        dispatch(changeAge())
-      }}>나이바꿔~</button>
+      {/* 버튼을 누르면 hailey에서 ham으로 변경되는 것을 확인할 수 있음 */}
+      <h1>{state.name}</h1>
+      <button onClick={() => { dispatch(changeName()) }}>Name Change</button>
     </div>
   );
 }
--- React/혜민님 특강/reduxtoolkit/src/store.js
+++ React/혜민님 특강/reduxtoolkit/src/store.js
@@ -1,30 +1,53 @@
+// import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+// let user = createSlice({
+//     name: 'user',
+//     initialState: {name : 'zeon', age: 1 },
+//     reducers: {
+//         changeName(state) {
+//             state.name = 'zin';
+//         }
+//     }
+// })
+// let user2 = createSlice({
+//     name: 'user2',
+//     initialState: {name : 'zeon2', age: 20 },
+//     reducers: {
+//         changeAge(state,action) {
+//             state.age += action.payload;
+//         }
+//     }
+//   })
+
+// export let { changeName } = user.actions
+// export let { changeAge } = user2.actions
+
+// export default configureStore({
+//   reducer: {
+//     user: user.reducer,
+//     user2: user2.reducer
+//   }
+// })
+
 import { configureStore, createSlice } from '@reduxjs/toolkit'
 
 let user = createSlice({
-    name: 'user',
-    initialState: {name : 'zeon', age: 1 },
-    reducers: {
-        changeName(state) {
-            state.name = 'zin';
-        }
+  name: 'user',
+  initialState: { name: "zeon", age: 1 },
+  reducers: {
+    changeName(state) {
+      state.name = "zin" // name의 zeon를 zin으로 변경 
+    },
+    changeAge(state, actions) {
+      state.age += actions.payload
     }
+  }
 })
-let user2 = createSlice({
-    name: 'user2',
-    initialState: {name : 'zeon2', age: 20 },
-    reducers: {
-        changeAge(state,action) {
-            state.age += action.payload;
-        }
-    }
-  })
 
-export let { changeName } = user.actions
-export let { changeAge } = user2.actions
+export let { changeName, changeAge } = user.actions //위에서 만든 변경함수 export
 
 export default configureStore({
   reducer: {
-    user: user.reducer,
-    user2: user2.reducer
+    user: user.reducer //user createSlice state를 등록(useSelector에서 이곳에 등록되어 있는 state들을 받아옴)
   }
 })
