e1358abdbb57b263c1e3ee81e47aa40cfd7f2a5d
Eunnnnnnnn <0624pjh@gmail.com>
2023-06-12 09:48:50 +0900

    프로젝트

---
+++ React/myapp/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+import { LoginButton, FollowButton, FollowButtonSm } from './components/common/Button'
+import { ResetStyle } from './style/ResetStyle'
+import {GlobalStyle} from './style/GlobalStyle'
+import { NavBar } from './components/common/NavBar'
+import InputTest from './componentsTest/InputTest'
+import { Modal } from './components/common/Modal'
+import { Alert } from './components/common/Alert'
+
+function App() {
+  return (
+    <>
+        <ResetStyle />
+        <GlobalStyle />
+        <LoginButton contents={'로그인'}/>
+        <FollowButton contents={'팔로우'}/>
+        <FollowButtonSm contents={'팔로우'}/>
+        <NavBar />
+        <InputTest />
+        <Modal />
+        <Alert />
+    </>
+
+  )
+}
+
+export default App
