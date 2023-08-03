2df9d5919c83b3b6da95c674d3f82db3282bd2c6
Eunnnnnnnn <0624pjh@gmail.com>
2023-08-04 06:01:48 +0900

    프로젝트 복구

--- React/myapp/src/pages/ChatRoomPage.jsx
+++ React/myapp/src/pages/ChatRoomPage.jsx
@@ -43,7 +43,6 @@ export default function ChatRoom() {
   return (
     <>
       <ChatHeader name={'바베큐러버'} isButton={true} handleFunc={openModal} />
-      <TopBtn />
       <ChatRoomPageStyle>
         {chatHistory.map((item, index) =>
           item.receive ? (
