6ec123ac32e81fd04bdddda7b00c62bdcb24c256
Eunnnnnnnn <0624pjh@gmail.com>
2023-08-30 23:47:54 +0900

    채팅기능 구현

--- React/myapp/src/pages/ChatRoomPage.jsx
+++ React/myapp/src/pages/ChatRoomPage.jsx
@@ -1,5 +1,5 @@
-import styled from 'styled-components';
-import { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
+import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import useModalControl from "../hooks/useModalControl";
@@ -12,98 +12,94 @@ import { Modal } from './../components/common/Modal';
 import SaleItem from '../assets/img/saleItem.png';
 
 export default function ChatRoom() {
-  
-  const [chatHistory, setChatHistory] =  useState([
+  const [chatHistory, setChatHistory] = useState([
     {
       Msg: '캠핑 아이스 박스 사고 싶어서 문의드려요. 혹시 상태가 괜찮은가요? 짐도 많이 들어가는 편인지 궁급합니다. 고기도 함께 구입하고 싶어요. 다음주에 캠핑을 가야해서 급하게 연락합니다.',
-      creatAt: '12:39',
+      createdAt: '오후 12:39',
       receive: true,
     },
     {
       Msg: '이번에 고기 언제 들어와요?',
-      creatAt: '12:41',
+      createdAt: '오후 12:41',
       receive: true,
     },
     {
       Msg: '고기는 다 먹었는데요',
-      creatAt: '12:50',
+      createdAt: '오후 12:50',
     },
     {
       Img: SaleItem,
-      creatAt: '12:51',
+      createdAt: '오후 12:51',
     },
   ]);
 
-  const {openModal, ModalComponent} = useModalControl();
+  const chatContainerRef = useRef(null);
+
+  const { openModal, ModalComponent } = useModalControl();
   const navigate = useNavigate();
   const { handleImageChange, imageURL, imagePath, uploadValidity } = useImageUploader();
   const [chatMessage, setChatMessage] = useState('');
 
-  const handleChatRoomOut = () =>{
+  const handleChatRoomOut = () => {
     navigate(-1)
   }
 
   const handleSendButtonClick = () => {
-    // 메시지 전송 처리 로직 구현
     const newChat = {
-      Msg: chatMessage,
-      createdAt: null, // 새로운 메시지의 생성 시간을 null로 설정
-      };
+      Msg: chatMessage, 
+      createdAt: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) 
+    };
+    if (chatMessage !== '') {
       setChatHistory([...chatHistory, newChat]);
       setChatMessage('');
-    };
-    
-    useEffect(() => {
-    // 이미지 업로드 처리 로직
-      if (uploadValidity) {
-        const newChat = {
-          Img: imageURL,
-          createdAt: null,
-        };
-        setChatHistory([...chatHistory, newChat]);
       }
-    }, [uploadValidity]);
+  };
 
-    useEffect(() => {
-      // chatHistory가 업데이트될 때 실행
-      // chatHistory의 마지막 메시지의 생성 시간을 갱신
-      const updatedChatHistory = chatHistory.map((item, index) => {
-        if (index === chatHistory.length - 1 && item.createdAt === null) {
-          // 새로운 메시지의 생성 시간을 현재 시간으로 설정
-          return { ...item, createdAt: new Date().toLocaleTimeString() };
-        }
-        return item;
-      });
-      setChatHistory(updatedChatHistory);
-    }, [chatHistory]);
+  useEffect(() => {
+    if (imagePath) {
+      const newChat = {
+        Img: imagePath,
+        createdAt: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) 
+      };
+      setChatHistory([...chatHistory, newChat]);
+    }
+  }, [imagePath]); 
+
+ useEffect(() => {
+    if (chatContainerRef.current) {
+      chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+    }
+  }, [chatHistory]);
 
   return (
     <>
       <ChatHeader name={'바베큐러버'} isButton={true} handleFunc={openModal} />
-      <ChatRoomPageStyle>
+      <ChatRoomPageStyle ref={chatContainerRef} >
         {chatHistory.map((item, index) =>
           item.receive ? (
             <ChatContainerStyle key={index}>
               <ProfileSm />
               <p>{item.Msg}</p>
-              <span className="time">{item.creatAt}</span>
+              <span className="time">{item.createdAt}</span>
             </ChatContainerStyle>
           ) : (
             <MyChatContainerStyle key={index}>
-              <span className="time">{item.creatAt}</span>
+              <span className="time">{item.createdAt}</span>
               {!!item.Img || <p>{item.Msg}</p>}
-              <img src={item.Img} alt="" />
+              {item.Img && <img src={item.Img} alt="" />}
             </MyChatContainerStyle>
           )
         )}
+      </ChatRoomPageStyle>
         <SendStyle aria-label='전송'>
           <div className='upload' >
-            <FileUploadSm id="uploading-img" onChange={handleImageChange} aria-label='파일 업로드' />
-          </div>
-          <InputStyle type={'text'} placeholder='메시지 입력하기...' value={chatMessage} onChange={(e) => setChatMessage(e.target.value)} aria-label='텍스트 입력'></InputStyle>
-          <button id='send' onClick={handleSendButtonClick} aria-label='전송 버튼'>전송</button>
+            <FileUploadSm id="uploading-img" onChange={handleImageChange} aria-label='파일 업로드' /> 
+          </div>  
+          <InputStyle type={'text'} placeholder='메시지 입력하기...' value={chatMessage} onChange={(e) => setChatMessage(e.target.value)} onKeyDown={(e) => {
+            if (e.key === 'Enter') {handleSendButtonClick();}}}
+aria-label='텍스트 입력' autoFocus />
+          <button id='send' onClick={handleSendButtonClick} disabled={!chatMessage} aria-label='전송 버튼'>전송</button>
         </SendStyle>
-      </ChatRoomPageStyle>
       <ModalComponent>
         <Modal contents={['채팅방 나가기']} handleFunc={handleChatRoomOut} />
       </ModalComponent>
@@ -115,7 +111,13 @@ const ChatRoomPageStyle = styled.div`
   position: relative;
   height: var(--screen-height);
   padding-top: 24px;
-
+  padding-bottom: 76px;
+  overflow-y: scroll;
+  ::-webkit-scrollbar {
+    background-color: var(--background-color);
+    width: 0px;
+  }
+  
   @media (min-width: 768px) {
     height: var(--screen-nav-height);
   }
@@ -162,8 +164,12 @@ const MyChatContainerStyle = styled.div`
     font-size: var(--font--size-md);
     color: var(--common-text-color-1);
   }
+  
 
   img {
+    width: 140px;
+    height: 115px;
+    object-fit: cover;
     border-radius: 10px;
   }
 
